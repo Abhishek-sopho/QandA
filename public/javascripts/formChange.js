3f03ae7d5966c3051892c2c2e0fac49aa6c81611
Saurabh Kumar <saurabh.k0402@gmail.com>
2016-01-27 23:32:25 +0530

    Add Title bar and menu bar basic structure

--- public/javascripts/formChange.js
+++ public/javascripts/formChange.js
@@ -1,30 +1,30 @@
 var span = document.getElementById("divFormChange"),
 	signIn = document.getElementsByClassName("divSignIn")[0],
 	signUp = document.getElementsByClassName("divSignUp")[0],
+	menu = document.getElementById("divMenu"),
+	title = document.getElementsByClassName("divTitle")[0],
 	count = 0;
 
 span.addEventListener("click", function(){
 	count++;
 	console.log(count);
 	if(count == 1) {
-		//signUp.style.left = "35%";
-		//signUp.style.transform = "rotateY(0deg)";
 		signUp.className = "divSignUp signUpStage2"
-		//signIn.style.transform = "rotateY(120deg)";
-		//signIn.style.left = "-80%";
 		signIn.className = "divSignIn signInStage2"
 		span.innerHTML = "Already  have  an  account <span id = 'spanTextForm'> Log In </span>";
 	}
 
 	if(count == 2) {
 		count = 0;
-		//signIn.style.left = "35%";
-		//signIn.style.transform = "rotateY(0deg)";
-		//signUp.style.left = "145%";
-		//signUp.style.transform = "rotateY(-120deg)";
 		signUp.className = "divSignUp"
 		signIn.className = "divSignIn"
 		span.innerHTML = "Don't  have  an  account <span id = 'spanTextForm'> Create an account </span>";
 	}
 
+}, false);
+
+document.getElementsByClassName("divMenuButton")[0].addEventListener("click", function(){
+	title.className = (title.className == "divTitle")?"divTitle divTitleColored":"divTitle"
+	this.className = (this.className == "divMenuButton")?"divMenuButton divMenuButtonColored":"divMenuButton";
+	menu.className = (menu.className == "divMenuNotVisible")?"divMenuVisible":"divMenuNotVisible";
 }, false);
